3fd6997b4bae9e9504c33aa2595e56685de24abf
GoelVaibhav009 <Creativeprogrammer623@gmail.com>
2020-06-18 17:53:23 +0530

    Added cart_list.dart Added shop_list.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'shop_list.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
+    return MaterialApp(
+      title: 'ShopX',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: ShopListWidget(),
     );
   }
 }
